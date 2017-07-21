033569675786150918aa85a11d3622a29848a12d
Kanokwan Sudleam <kanokwan.sud@kasikornbank.com>
2017-07-21 15:40:39 +0700

    frist

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -1,19 +1,57 @@
 package exam1;
 
 public class Exam01 {
+    int scoreA =0;
+    int scoreB = 0;
+    int checkroundA = 0;
+    int checkroundB = 0;
 
     public Exam01(String firstPerson, String secondPerson) {
     }
 
 
     public void playerAWin() {
+        checkroundA+=1;
+        scoreA+=15;
+
+             if(checkroundA == 3){
+                 scoreA =40;
+             }
+
     }
 
     public void playerBWin() {
+        checkroundB+=1;
+        scoreB+=15;
+
+        if(checkroundB == 3){
+            scoreB =40;
+        }
     }
 
     public String getScore() {
-        return null;
+        if(scoreA==15){
+            return "Fifteen-Love";
+        }
+        if(scoreA == 30){
+            return "Thirty-Love";
+        }if(scoreA == 40){
+            return "Forty-Love";
+        }if(scoreA >40){
+            return "Win for Player A";
+        }
+
+        if(scoreB == 15){
+            return "Love-Fifteen";
+        }
+        if(scoreB ==30) {
+            return "Love-Thirty";
+        }if(scoreA == 40){
+            return "Love-Forty";
+        }if(scoreA >40){
+            return "Win for Player B";
+        }
+        return "Love-All";
     }
 
 }
